128ef51a49bceaf7175e445390d5244ce77f16e5
muham4ik <muham4ik@example.com>
2024-06-23 21:33:52 +0500

    fixed

--- src/pages/Sign-In/index.jsx
+++ src/pages/Sign-In/index.jsx
@@ -4,8 +4,10 @@ import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import { useState } from 'react';
 import {auth} from "@service"
+import { useNavigate } from 'react-router-dom';
 
 const index = () => {
+  const navigate = useNavigate()
   const [form,setForm] = useState({})
   const handleSubmit= async(e)=>{
     e.preventDefault();
@@ -16,6 +18,9 @@ const index = () => {
     }catch(error){
         console.log(error);
     }
+}
+const handleClik =()=>{
+  navigate("/sign-up")
 }
 const handleChange = (e)=>{
 const {name , value} = e.target
@@ -28,7 +33,7 @@ setForm({...form , [name]:value})
     <form onSubmit={handleSubmit} className='flex flex-col justify-center  m-auto gap-3 w-[500px]'>
     <TextField fullWidth onChange={handleChange} label="email" id="fullWidth" name='email'/>
     <TextField fullWidth onChange={handleChange} label="password" id="fullWidth" name='password'/>
-    <a href="">Register</a>
+    <button onClick={handleClik} className='bg-transparent outline-none border-none text-start cursor-pointer w-[50px]'>Register</button>
     <Button variant="contained" className='w-full' type='submit' endIcon={<SendIcon/>}>Submit</Button>
     </form>
    </div>
